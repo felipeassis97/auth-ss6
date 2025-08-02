61158a6d599d8fe614c9819b6db545b5f410175d
felipeassis97 <felipeassis97@gmail.com>
2025-08-02 11:04:37 +1000

    feat: initial commit

---
+++ src/main/java/felipeassisdev/authss6/Authss6Application.java
@@ -0,0 +1,13 @@
+package felipeassisdev.authss6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authss6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Authss6Application.class, args);
+	}
+
+}
---
+++ src/test/java/felipeassisdev/authss6/Authss6ApplicationTests.java
@@ -0,0 +1,13 @@
+package felipeassisdev.authss6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Authss6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
